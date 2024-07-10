0883b79ca81a8ace8db2acfd9f1eb70a06140226
Joe-Augustus-Fernando <joeaugustusfernando@gmail.com>
2024-07-10 17:49:40 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,22 @@
+permission=input("Enter if you want to play a game or not")
+
+if permission=='yes'or'ok':    
+ import random as rd
+
+ var=rd.randint(1,6)
+
+ v=int(input("Enter a no from 1 to 6:"))
+
+ if var==v:
+    print("You\'ve guessed it correctly")
+    
+ else:
+    print("Better luck next time")
+    
+ print(var)
+
+elif permission=='no':
+    print("Goodbye!")
+
+else:
+    print("invalid")
